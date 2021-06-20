f30161c2600daab64ab8a363731ddc9f6f9c6dd7
venusdharan <venusdharan@gmail.com>
2021-06-20 22:43:41 +0530

    build url

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -148,8 +148,8 @@ export default (ctx, inject) => {
   const runtimeConfig = ctx.$config && ctx.$config.axios || {}
   // baseURL
   const baseURL = process.browser
-    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'https://compass-attendence.herokuapp.com/')
-      : (runtimeConfig.baseURL || runtimeConfig.baseUrl || process.env._AXIOS_BASE_URL_ || 'https://compass-attendence.herokuapp.com/')
+    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'http://142.93.209.130:5000/')
+      : (runtimeConfig.baseURL || runtimeConfig.baseUrl || process.env._AXIOS_BASE_URL_ || 'http://142.93.209.130:5000/')
 
   // Create fresh objects for all default header scopes
   // Axios creates only one which is shared across SSR requests!
